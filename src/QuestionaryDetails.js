ca943eeb72aa26093a3d0e2aa70d047e4f1393c6
LuisIzarra <lrodriguezizarra@gmail.com>
2022-09-01 18:33:59 -0500

    adds video and audio recorder

--- src/QuestionaryDetails.js
+++ src/QuestionaryDetails.js
@@ -1,24 +1,40 @@
 import Questionary from "./Questionary";
 import { Link } from "react-router-dom";
 import { Box } from "@mui/material";
+import { useRef } from "react";
 
 const QuestionaryDetails = () => {
+  const videoRef = useRef();
+
   const _handleOnClickPlay = () => {
     if (navigator.mediaDevices) {
       console.log("getUserMedia supported.");
-      const constraints = { audio: true };
-      let chunks = [];
+      const constraints = { video: true, audio: true };
 
       navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
-        const mediaRecorder = new MediaRecorder(stream);
+        videoRef.current.srcObject = stream;
       });
     }
+  };
+
+  const _handleCanPlay = () => {
+    videoRef.current.play();
+    console.log("_handleCanPlay");
   };
 
   return (
     <Box>
       <Link to="/">Volver</Link>
       <Questionary onClickPlay={_handleOnClickPlay} />
+      <video
+        ref={videoRef}
+        onCanPlay={_handleCanPlay}
+        width="200px"
+        height="200px"
+        autoPlay
+        playsInline
+        muted
+      ></video>
     </Box>
   );
 };
